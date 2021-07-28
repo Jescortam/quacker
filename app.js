f4972d7875e3de527f0fe068856a85cf792a2c12
Jesus Cornejo <jesus.cornejo71002@gmail.com>
2021-07-28 17:57:46 -0500

    Actually added update and delete functionality to post this time

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const Post = require('./models/posts');
-const engine = require('ejs-mate')
+const engine = require('ejs-mate');
+const methodOverride = require('method-override');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/quacker', {
     useNewUrlParser: true, 
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: true
 });
 
 const db = mongoose.connection;
@@ -16,12 +18,13 @@ db.once('open', function() {
     console.log('Successfully connnected to DB')
 });
 
-app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'ejs')
-app.engine('ejs', engine)
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.engine('ejs', engine);
 
-app.use(express.static(path.join(__dirname, '/public')))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -37,15 +40,32 @@ app.get('/posts/new', (req, res) => {
 })
 
 app.post('/posts', async (req, res) => {
-    const post = new Post(req.body.post)
-    post.date = new Date(Date.now())
-    await post.save()
-    res.redirect('/posts');
+    const post = new Post(req.body.post);
+    post.date = new Date(Date.now());
+    await post.save();
+    res.redirect(`/posts/${post._id}`);
 })
 
 app.get('/posts/:id', async (req, res) => {
     const post = await Post.findById(req.params.id);
-    res.render('posts/show', { post })
+    res.render('posts/show', { post });
+})
+
+app.get('/posts/:id/edit', async (req, res) => {
+    const post = await Post.findById(req.params.id);
+    res.render('posts/edit', { post });
+})
+
+app.put('/posts/:id', async (req, res) => {
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+    post.date = new Date(Date.now());
+    await post.save();
+    res.redirect(`/posts/${post._id}`);
+})
+
+app.delete('/posts/:id', async (req, res) => {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    res.redirect('/posts')
 })
 
 app.listen(3000, (req, res) => {
