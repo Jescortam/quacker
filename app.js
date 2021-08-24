8a56f75fcac377d862ea1eacc498e1c3a1e17543
Jesus Cornejo <jesus.cornejo71002@gmail.com>
2021-08-24 12:20:36 -0500

    Added security and now trying to deploy

--- app.js
+++ app.js
@@ -17,13 +17,17 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/users');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const MongoStore = require('connect-mongo');
 
 const postRoutes = require('./routes/posts');
 const commentRoutes = require('./routes/comments');
 const userRoutes = require('./routes/users');
 
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/quacker';
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/quacker', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useFindAndModify: false,
@@ -43,18 +47,63 @@ app.engine('ejs', engine);
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
+app.use(mongoSanitize({ replaceWith: '_' }));
+
+const store = new MongoStore({
+    mongoUrl: dbUrl,
+    secret: process.env.SESSION_SECRET,
+    touchAfter: 24 * 3600
+})
+
+store.on("error", function (e) {
+    console.log("SESSION STORE ERROR", e)
+})
+
 app.use(session({
-    secret: 'quackquackquack',
+    name: 'session',
+    store,
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 3600000 * 24 }
+    cookie: { 
+        httpOnly: true,
+        maxAge: 3600000 * 24 
+    }
 }))
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(helmet())
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+const scriptSrcUrls = [];
+const styleSrcUrls = [
+    "https://fonts.googleapis.com/",
+];
+const connectSrcUrls = [];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dx5veymx5/",
+                "https://images.unsplash.com/"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
--- schemas.js
+++ schemas.js
@@ -1,16 +1,39 @@
-const Joi = require('joi')
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 const postSchema = Joi.object({
     post: Joi.object({
-        body: Joi.string().required().trim().max(63206).truncate()
-    }),
+        body: Joi.string().required().trim().max(2048).truncate().escapeHTML()
+    }).required(),
     image: Joi.binary().encoding('base64'),
     deleteImgs: Joi.array()
 }).required()
 
 const commentSchema = Joi.object({
     comment: Joi.object({
-        body: Joi.string().required().trim().max(63206).truncate()
+        body: Joi.string().required().trim().max(2048).truncate().escapeHTML()
     }).required()
 }).required()
 
